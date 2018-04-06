7a46e0703ed88cbbf57c2dd9aab6ac6155929bef
TDW <tdw@tdw.de>
2018-04-06 08:21:08 +0200

    rotation eingebaut

--- image_with_caption.py
+++ image_with_caption.py
@@ -6,31 +6,38 @@ from PIL import Image, ImageDraw, ImageFont
 
 def add_caption(input_file, text, output_file="result.jpg"):
     image = Image.open(input_file) # open original
+    path_fonts = "/usr/share/fonts/TTF/"
     exif_data = image._getexif() # get exif data
 
     if exif_data: # build caption
         image_date = exif_data[306] #306 is DateTime
-        # exif 274 is imagerotation
         caption = text + "   " + str(image_date)
+        image_rotation = exif_data[274] # exif 274 is imagerotation
+        if image_rotation == 1:
+            image = image.rotate(90, expand=True) # rotate 90 deg counterclockwise
+        elif image_rotation == 3:
+            image = image.rotate(-90, expand=True) # rotate 90 deg clockwise
+        elif image_rotation == 8:
+            image = image.rotate(180, expand=True) # rotate 180 deg counterclockwise
     else:
         caption = text
 
-    image = image.rotate(90, expand=True) # rotate 90 deg counterclockwise
-    size_image = numpy.array(image.size) # get size of picture
-
-    path_fonts = "/usr/share/fonts/TTF/"
-    size_font = int(size_image[1]*0.01) # calculate optimal fontsize
-    font = ImageFont.truetype(path_fonts+"DejaVuSans.ttf", size_font)
-
+    size_image = numpy.array(image.size)
+    size_font = int(max(size_image) * 0.01)
+    font = ImageFont.truetype(path_fonts + "DejaVuSans.ttf", size_font)
     size_text = numpy.array(font.getsize(caption)) # get size of text
+    margin = int(min(size_text) * 0.3)
 
-    margin = int(size_text[1]*0.3)
-    size_blank_image = (size_image[0], size_image[1] + size_text[1] + margin*2) #gnügend Platz für den text
+    # abhähgig von orientierung
+    if image_rotation == 1 or image_rotation == 3:
+        size_blank_image = (min(size_image), max(size_image) + min(size_text) + margin*2) #gnügend Platz für den text
+    elif image_rotation == 6 or image_rotation == 8:
+        size_blank_image = (max(size_image), min(size_image) + min(size_text) + margin*2) #gnügend Platz für den text
+
     blank_image = Image.new('RGB', size_blank_image, color="#000000") # new blank image
-
     canvas = ImageDraw.Draw(blank_image)
-    canvas.text((0 + margin*5, 0 + margin), caption, font=font, fill="#ffffff") # text einfügen
-    blank_image.paste(image, (0, 0 + size_text[1] + margin*2)) #original bild einfügen
+    canvas.text((0 + margin * 5, 0 + margin), caption, font=font, fill="#ffffff") # text einfügen
+    blank_image.paste(image, (0, 0 + min(size_text) + margin * 2)) #original bild einfügen
     blank_image = blank_image.rotate(-90, expand=True) # zurück rotieren
     blank_image.save(output_file, exif=image.info['exif'])
 
